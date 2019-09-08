5d4e2a9be97068e552105bcfdce13a4a9e6e5865
ElodKuthy <kuthyelod@gmail.com>
2019-09-08 16:17:48 +0200

    Material UI init

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
 const store = createStore(rootReducer);
+
+const theme = createMuiTheme({
+
+});
 
 function App() {
   return (
     <Provider store={store}>
-      <div />
+      <ThemeProvider theme={theme}>
+        <div />
+      </ThemeProvider>
     </Provider>
   );
 }
